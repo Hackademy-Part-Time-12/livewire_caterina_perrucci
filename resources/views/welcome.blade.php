d7c4562f732182fd756f8c1ba7fe31cf5859d942
Caterina-Perrucci <caterina.perrucci@aulab.it>
2023-11-06 21:31:42 +0100

    Counter e ricerca in real-time

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,20 @@
+<x-layout>
+    <livewire:counter />
+    <livewire:counter />
+</x-layout>
+{{--<script>
+    let number = document.querySelector('#number');
+    let increment = document.querySelector('#increment');
+    let decrement = document.querySelector('#decrement');
+    let value = number.innerHTML;
+    
+    increment.addEventListener('click', ()=>{
+        value++;
+        number.innerHTML = value;
+    });
+    
+    decrement.addEventListener('click', ()=>{
+        value--;
+        number.innerHTML = value;
+    });
+</script>--}}
